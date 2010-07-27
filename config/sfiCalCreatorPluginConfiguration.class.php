71f4c4387ae350444626ae3a18a85c117c88fdbc
andrelademann <andrelademann@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2010-07-27 18:02:13 -0500

    Add plugin configuaration file and directory.
    git-svn-id: http://svn.symfony-project.com/plugins/sfiCalCreatorPlugin/trunk@30436 ee427ae8-e902-0410-961c-c3ed070cd9f9

---
+++ config/sfiCalCreatorPluginConfiguration.class.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * sfiCalCreatorPlugin configuration.
+ * 
+ * @package     sfiCalCreatorPlugin
+ * @subpackage  config
+ * @author      Your name here
+ * @version     SVN: $Id: PluginConfiguration.class.php 17207 2009-04-10 15:36:26Z Kris.Wallsmith $
+ */
+class sfiCalCreatorPluginConfiguration extends sfPluginConfiguration
+{
+  const VERSION = '0.0.2-ALPHA';
+
+  /**
+   * @see sfPluginConfiguration
+   */
+  public function initialize()
+  {
+  }
+}
